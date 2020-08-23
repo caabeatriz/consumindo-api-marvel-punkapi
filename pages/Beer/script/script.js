eb5e7fb39c6457b75620edfbbeb91b6e01979705
carolhybank <caroline.silva@hybank.biz>
2020-08-22 22:22:45 -0300

    Adicionado funções de click e validações do input

--- pages/Beer/script/script.js
+++ pages/Beer/script/script.js
@@ -1,6 +1,29 @@
-fetch('https://api.punkapi.com/v2/beers?food=tacos')
-    .then (response => response.json())
-    .then( data =>
-        console.log(data)
-    )
-    .catach()
+const searchButton = document.querySelector(".search-button")
+
+//Para clicar no botão e trazer o valor do input
+searchButton.addEventListener('click', event => {
+    event.preventDefault();
+    validateInput()
+});
+
+// Para capturar o valor do input
+function captureSelect() {
+    var select = document.getElementById('filtersBeers');
+    var value = select.options[select.selectedIndex].value;
+    return value;
+}
+
+function validateInput() {
+
+    if(captureSelect() == "name"){
+        fetch('https://api.punkapi.com/v2/beers?food='+name)
+        .then(response => response.json())
+        .then(data =>
+            console.log(data)
+        )
+    .catch()
+    }
+    console.log(captureSelect())
+}
+
+
