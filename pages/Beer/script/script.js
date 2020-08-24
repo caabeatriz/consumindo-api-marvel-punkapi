e7054793595a1d8126a0d4b9ecd290eb93d57c8d
carolhybank <caroline.silva@hybank.biz>
2020-08-24 11:00:33 -0300

    Adicionando passo a passo de como criar os métodos para página 2

--- pages/Beer/script/script.js
+++ pages/Beer/script/script.js
@@ -21,9 +21,20 @@ function validateInput() {
         .then(data =>
             console.log(data)
         )
-    .catch()
+    .catch(
+        console.log("errorrrrrrr", error())
+        //Aqui é para adicionar uma função de tratar o erro
+        
+    )
     }
     console.log(captureSelect())
 }
 
-
+function error(){
+    //Criar um html com os dizeres 'Não encontramos o que você digitou' 
+    //Inserir umgif
+    return Teste ; 
+}
+function createInfo(){
+    //Criar aqui um html para criar as informalões de divs e limpar caso tenha a div de erro
+}
