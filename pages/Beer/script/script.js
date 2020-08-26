599ac7bb2916e327071d3ae21111acabfac4e570
carolhybank <caroline.silva@hybank.biz>
2020-08-26 00:50:12 -0300

    Adicionando função para setar local storage com um array

--- pages/Beer/script/script.js
+++ pages/Beer/script/script.js
@@ -1,6 +1,8 @@
 const searchButton = document.querySelector(".search-button")
 const container = document.querySelector('.beersInformation')
+const containerFav = document.querySelector('.favorites')
 var beers;
+const favorites = []
 // fetch('https://api.punkapi.com/v2/beers?beer_name=punk')
 // .then(response => response.json())
 // .then(data => {
@@ -10,6 +12,11 @@ var beers;
 //     .catch(err => console.error(err))
 
 //Para clicar no botão e trazer o valor do input
+
+function startApp() {
+    createAboutBeers()
+}
+
 searchButton.addEventListener('click', event => {
     event.preventDefault();
     request()
@@ -23,21 +30,21 @@ function captureSelect() {
     return value;
 }
 // Para capturar o valor do select
-function captureInput () {
+function captureInput() {
     var inputValue = document.querySelector('#searchInputBeer').value
     return inputValue;
 }
 
 //Para validar o select
-function validateSelect(){
+function validateSelect() {
     var url
     var valueInput = captureInput();
 
-    if(captureSelect() == 'name'){
-        url = 'https://api.punkapi.com/v2/beers?beer_name='+valueInput; 
+    if (captureSelect() == 'name') {
+        url = 'https://api.punkapi.com/v2/beers?beer_name=' + valueInput;
 
-    } else if (captureSelect() == 'food'){
-        url ='https://api.punkapi.com/v2/beers?food='+valueInput;
+    } else if (captureSelect() == 'food') {
+        url = 'https://api.punkapi.com/v2/beers?food=' + valueInput;
     }
 
     return url
@@ -45,31 +52,30 @@ function validateSelect(){
 
 
 // Para entrar na api
-function request (){
+function request() {
     var requestUrl = validateSelect()
     fetch(requestUrl)
-    .then(response => response.json())
-    .then(data => {
-        beers= data
-        createAboutBeers()
-        teste()
-    })
-    .catch(err => console.error('erro'))
+        .then(response => response.json())
+        .then(data => {
+            beers = data
+            startApp()
+        })
+        .catch(err => console.error('erro'))
 
 }
 
-function error(){
+function error() {
     //Criar um html com os dizeres 'Não encontramos o que você digitou' 
     //Inserir umgif
     var teste = "erro"
-    return teste ; 
+    return teste;
 }
 
-function createAboutBeers(){
+function createAboutBeers() {
     //Criar aqui um html para criar as informalões de divs e limpar caso tenha a div de erro
     // Coloca todos os campos, mas os q estiverem vazios deixa em branco
-    if (captureSelect() == 'food'){
-        for (var i = 0 ; i < beers.length; i++){
+    if (captureSelect() == 'food') {
+        for (var i = 0; i < beers.length; i++) {
             card = `
                 <div class=" card beer-info">
                 <div class="card-body">
@@ -79,40 +85,54 @@ function createAboutBeers(){
                     <p class="card-text">  ${beers[i].description}</p>
                     <img class="card-img" src="${beers[i].image_url}" alt""> 
                 </div>`
-            ;
+
+                ;
             container.innerHTML += card;
-            
+
         }
     }
-    
-    else if (captureSelect() == 'name' ){
-        for (var i = 0 ; i < beers.length; i++){
+
+    else if (captureSelect() == 'name') {
+        for (let i = 0; i < beers.length; i++) {
             card = `
                 <div class=" card beer-info">
-             <button  class="starButton" > starrrr </button>
+             <button  class="starButton" onclick="setLocal()" data-index=${i} > starrrr </button>
                 <div class="card-body">
                     <h4 class="card-title">Name: ${beers[i].name}  </h4>
                     <p class="card-text">  ${beers[i].description}</p>
                     <img class="card-img" src="${beers[i].image_url}" alt""> 
                 </div>`
-            ;
+
+                ;
             container.innerHTML += card;
-        }    
+        }
     }
-    
+
     return card;
 }
 
 // ao clicar na estrela salva uma classe para favoritar
+function setLocal() {
+    let index = event.target.dataset.index;
+    var verification = favorites.includes(beers[index].name);
 
+    if (verification == false) {
+        favorites.push(beers[index].name);
+    } 
+    else {
+    }
+    
+    localStorage.setItem('name', favorites)
+}
+    // incluir o resultado e entrar no laço dnv e verificar se resultado ja existe se nao exister inclui
 
+    // for(let i = 0 ; i < favorites1.length; i ++){
+    //     console.log("teste", favorites1[i])
+    //     card = `
+    //      <div class=""> testetete </div>
+    //     `
+    // }
 
-function teste (){
-const buttonStar = document.querySelector('.starButton')
-console.log(buttonStar)
-buttonStar.addEventListener('click', event => {
-    event.preventDefault();
-    console.log("teste")
-    localStorage.setItem('nome','Jack Sparrow')
+    // se o valor ja estiver no local storage, não adiciona dnv
 
-})}
+    // containerFav.innerHTMLa+=  card;
